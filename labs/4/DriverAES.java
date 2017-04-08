feeb552803ca962b42dcca6356de9a896b8bad60
Genti <gentius2@gmail.com>
2017-04-08 19:11:45 -0400

    Changed header for DriverAES.java

--- labs/4/DriverAES.java
+++ labs/4/DriverAES.java
@@ -1,9 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
+/**
+ * file: DriverAES.java
+ * author: Gentjan Gjeci
+ * course: MSCS630
+ * assignment: lab4
+ * due date: 04-05-2017
+ * version: 1.0
+ * <p>
+ * This file has driver class to display all AES round keys.
+ */
 import java.util.Scanner;
 
 /**
- * Created by genti on 4/6/2017.
+ * DriverAES
+ *
+ * This is the driver class which creates round keys for AES
  */
 public class DriverAES {
   public static void main(String[] args){
